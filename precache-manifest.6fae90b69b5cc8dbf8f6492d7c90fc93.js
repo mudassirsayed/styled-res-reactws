bb22e0aa1490ffe3a1af71a8d1a262b5b74b1426
mudassirsayed <sayedmudassir37@gmail.com>
2020-09-25 13:09:11 +0530

    Updates

---
+++ precache-manifest.6fae90b69b5cc8dbf8f6492d7c90fc93.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e15b9b6522cee041754b2ca57091ed72",
+    "url": "/styled-res-reactws/index.html"
+  },
+  {
+    "revision": "4ce80c06f832dc348bb2",
+    "url": "/styled-res-reactws/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "474bc394af8bb865cdaf",
+    "url": "/styled-res-reactws/static/js/2.a625b3e4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/styled-res-reactws/static/js/2.a625b3e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ce80c06f832dc348bb2",
+    "url": "/styled-res-reactws/static/js/main.4d4dba01.chunk.js"
+  },
+  {
+    "revision": "0ceaf916df3e322338f7",
+    "url": "/styled-res-reactws/static/js/runtime-main.aeede3e1.js"
+  },
+  {
+    "revision": "79ba2002d56019901132986d5c84067a",
+    "url": "/styled-res-reactws/static/media/profile.79ba2002.jpg"
+  },
+  {
+    "revision": "55cfb4817b9e054491bd9dafc21087c2",
+    "url": "/styled-res-reactws/static/media/svg-1.55cfb481.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "/styled-res-reactws/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "397c455a49d4f872c292a3dc25432579",
+    "url": "/styled-res-reactws/static/media/svg-3.397c455a.svg"
+  }
+]);
